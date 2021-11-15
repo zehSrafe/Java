3dcc479f78ce4469c13d3f13ae9a08281507422c
LoicDeketelaerePXL <12000701@student.pxl.be>
2021-11-15 14:46:59 +0100

    lost count test from IJ

--- EenvoudigeKlassen/src/be/intec/DurationChronounitPeriodApp.java
+++ EenvoudigeKlassen/src/be/intec/DurationChronounitPeriodApp.java
@@ -16,7 +16,5 @@ public class DurationChronounitPeriodApp {
         System.out.println("Total amount of weeks: " + ChronoUnit.WEEKS.between(birthDate, nowDate));
         System.out.println("Total amount of months: " + ChronoUnit.MONTHS.between(birthDate, nowDate));
         System.out.println("Total amount of months: " + periodSinceBirth.toTotalMonths());
-
-        //some text for a git Push
     }
 }
