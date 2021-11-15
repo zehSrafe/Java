66b3521862a88306ce0181c7a4184367edae9547
LoicDeketelaerePXL <12000701@student.pxl.be>
2021-11-15 14:36:21 +0100

    Third test from IJ

--- EenvoudigeKlassen/src/be/intec/WrapperAppApp.java
+++ EenvoudigeKlassen/src/be/intec/WrapperAppApp.java
@@ -1,2 +1,12 @@
-package be.intec;public class WrapperAppApp {
+package be.intec;
+
+public class WrapperAppApp {
+    public static void main(String[] args) {
+        int myInt = 88;
+        Integer myInteger = Integer.valueOf(myInt);
+
+        System.out.println("Integer: " + myInteger);
+        System.out.println("Bit: " + Integer.bitCount(myInteger));
+        System.out.println("Byte: " + myInteger.byteValue());
+    }
 }
