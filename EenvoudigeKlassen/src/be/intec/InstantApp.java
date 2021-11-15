d55ca2782d7d679602c50891fee7e585e4e37e73
LoicDeketelaerePXL <12000701@student.pxl.be>
2021-11-15 14:03:10 +0100

    test from git bash

--- EenvoudigeKlassen/src/be/intec/InstantApp.java
+++ EenvoudigeKlassen/src/be/intec/InstantApp.java
@@ -5,12 +5,13 @@ import java.time.Instant;
 public class InstantApp {
     public static void main(String[] args) {
         Instant currentTime = Instant.now();
-        System.out.println("Current: " + currentTime);
         Instant currentTimePlusSeven = currentTime.plusSeconds(7);
-        System.out.println("Current + 7s: " + currentTimePlusSeven);
         Instant currentTimePlusFive = currentTimePlusSeven.plusMillis(5);
-        System.out.println("Current + 5ms: " + currentTimePlusFive);
         Instant currentTimePlusThree = currentTimePlusFive.plusNanos(3);
+
+        System.out.println("Current: " + currentTime);
+        System.out.println("Current + 7s: " + currentTimePlusSeven);
+        System.out.println("Current + 5ms: " + currentTimePlusFive);
         System.out.println("Current + 3ns: " + currentTimePlusThree);
     }
 }
---
+++ EenvoudigeKlassen/src/be/intec/WrapperAppApp.java
@@ -0,0 +1,2 @@
+package be.intec;public class WrapperAppApp {
+}
