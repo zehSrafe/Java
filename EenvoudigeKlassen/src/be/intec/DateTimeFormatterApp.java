c675157c5134e802a51b98e0e09d143a8ac4e26b
Loïc Deketelaere <zehsrafe@gmail.com>
2021-11-15 15:13:06 +0100

    Should be resolved

--- EenvoudigeKlassen/src/be/intec/DateTimeFormatterApp.java
+++ EenvoudigeKlassen/src/be/intec/DateTimeFormatterApp.java
@@ -2,10 +2,13 @@ package be.intec;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class DateTimeFormatterApp {
     public static void main(String[] args) {
-        String date = "04/04/2020";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a date in a format DD/MM/YYYY");
+        String date = input.next();
         DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         LocalDate convertedDate = LocalDate.parse(date, myFormat);
